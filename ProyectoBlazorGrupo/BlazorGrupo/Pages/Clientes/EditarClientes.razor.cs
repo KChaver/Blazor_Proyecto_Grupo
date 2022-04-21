03f196f0aa636def2ec2b0a4d5e275c0012f1173
KChaver <kevinchaver14@gmail.com>
2022-04-21 12:09:19 -0600

    correcion de error
    Correcion de error

---
+++ ProyectoBlazorGrupo/BlazorGrupo/Pages/Clientes/EditarClientes.razor.cs
@@ -0,0 +1,14 @@
+﻿using BlazorGrupo.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorGrupo.Pages.Clientes;
+
+partial class EditarClientes
+{
+    [Inject] private IClientesServicio _clienteServicio { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+       
+    }
+}
